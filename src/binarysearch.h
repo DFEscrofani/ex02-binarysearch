2e2d3c412109df01ca166015833aac478fe56adc
VC Science Labs <vscilab@vcccd.edu>
2018-09-28 13:51:01 -0700

    turning in

---
+++ src/binarysearch.h
@@ -0,0 +1,28 @@
+/**
+* CS V15 Data Structures and Algorithms
+* CRN: 70342
+* Assignment: Binary Search
+*
+* Statement of code ownership: I hereby state that I have written all of this
+* code and I have not copied this code from any other person or source.
+*
+* @DaltonEscrofani
+*/
+
+#include <cstdint>
+
+namespace edu {	namespace vcccd {	namespace vc {	namespace csv15 {
+		template <class T>
+		int64_t binarysearch(int value, int array[], size_t size) {
+			if (size == 0)
+				return -1;
+			int guess = size / 2;
+			if (array[guess] == value)
+				return guess;
+			if (array[guess] < value)
+				return binarysearch(value, array, size / 2);
+			else if (array[guess] > value)
+				return binarysearch(value, array + guess + 1, guess);
+		return -1;
+		}
+}}}}
---
+++ src/main.cc
@@ -0,0 +1,21 @@
+/**
+* CS V15 Data Structures and Algorithms
+* CRN: 70342
+* Assignment: Binary Search
+*
+* Statement of code ownership: I hereby state that I have written all of this
+* code and I have not copied this code from any other person or source.
+*
+* @DaltonEscrofani
+*/
+
+#include <iostream>
+#include "binarysearch.h"
+
+using edu::vcccd::vc::csv15::binarysearch;
+
+int main(int argc, char *argv[]) {
+	int array[] = {1, 6, 7, 10, 12, 16};
+	binarysearch (10, array, 6);
+
+}
